b35eafc106708e0ed6c2b9df9b7406b09e79910c
Daniel Pavela <daniel.pavela@gmail.com>
2020-06-02 12:40:59 +1000

    Updated help menu to be actually useful

--- project.py
+++ project.py
@@ -34,7 +34,26 @@ if os.path.exists(ConfigFile) != True:
 	exit()
 
 def help():
-	print("help")
+	helpText = """
+project [-cdDghv] [project]
+
+Usage:
+  project [-g|--goto] <project>
+  project -l | --list
+  project -h | --help
+  project -v | --version
+
+Options:
+  -h --help         Show this screen.
+  -v --version      Show version.
+  -g --goto         Default: Goto project folder.
+  -c --create       Create project file.
+  -d --delete       Delete project file.
+  -D --deleteAll    Delete entire project folder.
+  -l --list         List all projects.
+	  """
+
+	print(helpText)
 
 def changeDir(arg):
 	path = "error"
